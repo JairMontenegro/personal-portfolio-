628a8d5fc4159420d8045438ff762134c4cc59ba
Jair montenegro <jairmontenegrof@gmail.com>
2022-06-01 19:49:04 -0300

    async await

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const flagsElement = document.getElementById("flagsc")
 
+const changeLanguage = async language=>{
+    const requestJson = await fetch(`./languages/${language}.json`)
+    const text = await requestJson.json();
+
+    console.log(text)
+}
 
 flagsElement.addEventListener("click",(e) => {
-    console.log(e.target.parentElement.dataset.language)
+    changeLanguage(e.target.parentElement.dataset.language)
+
 })

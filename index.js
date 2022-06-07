84c399d76368021723f4881e1816836c711815fa
Jair montenegro <jairmontenegrof@gmail.com>
2022-06-07 18:55:34 -0300

    i haven't found how i can fix hte problem

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const flagsElement = document.getElementById("flagsc")
 
+const textChange = document.querySelectorAll("[data-section]")
+
 const changeLanguage = async language=>{
     const requestJson = await fetch(`./languages/${language}.json`)
     const text = await requestJson.json();
 
-    console.log(text)
+ for(textChange of textChange){
+     console.log(textChange);
+ }
+
 }
 
 flagsElement.addEventListener("click",(e) => {

76c3ab49e5e1b02aabdc6f93bfab7239e0b8f312
Jair montenegro <jairmontenegrof@gmail.com>
2022-06-08 18:35:16 -0300

    fix the problem and finish change language

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-const flagsElement = document.getElementById("flagsc")
+const flagsElement = document.getElementById("flagsc");
+const textsChange = document.querySelectorAll("[data-section]");
 
-const textChange = document.querySelectorAll("[data-section]")
-
-const changeLanguage = async language=>{
+const changeLanguage = async language => {
     const requestJson = await fetch(`./languages/${language}.json`)
     const text = await requestJson.json();
 
- for(textChange of textChange){
-     console.log(textChange);
- }
+    for (const textsToChange of textsChange){
+        const section = textsToChange.dataset.section;
+        const value = textsToChange.dataset.value;
 
-}
+        textsToChange.innerHTML = text[section][value];
+    }
+
+
+};
+
+
+
 
 flagsElement.addEventListener("click",(e) => {
     changeLanguage(e.target.parentElement.dataset.language)

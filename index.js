8cc2b9c5492d02ea6e152ea925df1d1808c708dd
Jair montenegro <jairmontenegrof@gmail.com>
2022-12-01 21:51:35 -0300

    add 25 + 5 clock

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
 const flagsElement = document.getElementById("flagsc");
 const textsChange = document.querySelectorAll("[data-section]");
 
-const changeLanguage = async language => {
-    const requestJson = await fetch(`./languages/${language}.json`)
-    const text = await requestJson.json();
+const changeLanguage = async (language) => {
+  const requestJson = await fetch(`./languages/${language}.json`);
+  const text = await requestJson.json();
 
-    for (const textsToChange of textsChange){
-        const section = textsToChange.dataset.section;
-        const value = textsToChange.dataset.value;
+  for (const textsToChange of textsChange) {
+    const section = textsToChange.dataset.section;
+    const value = textsToChange.dataset.value;
 
-        textsToChange.innerHTML = text[section][value];
-    }
-
+    textsToChange.innerHTML = text[section][value];
+  }
 };
 
-
-
-
-flagsElement.addEventListener("click",(e) => {
-    changeLanguage(e.target.parentElement.dataset.language)
-
-})
+flagsElement.addEventListener("click", (e) => {
+  changeLanguage(e.target.parentElement.dataset.language);
+});
